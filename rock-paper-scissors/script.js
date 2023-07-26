b8a17d088a01c922ef4290e560a3d4a38e27d3e1
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-07-26 12:51:14 +0200

    Fixed buttons and div positions

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,3 +1,7 @@
+/**
+ * Randomizes computer's choice.
+ * @returns {string} computer's choice
+ */
 function getComputerChoice() {
     const randomChoice = Math.random();
 
@@ -9,12 +13,17 @@ function getComputerChoice() {
         return "astronaut";
 }
 
-/* Alien beats Spaceship.
-Spaceship beats Astronaut.
-Astronaut beats Alien. */
 
+/**
+ * Play's a round of rock paper scissors.
+ * @param playerSelection player's choice (input by clicking a button)
+ * @param computerSelection computer's choice (input by randomly generating)
+ * @returns {string} message with the result
+ */
 function playRound(playerSelection, computerSelection) {
-    // winning choices
+    /* Alien beats Spaceship.
+    Spaceship beats Astronaut.
+    Astronaut beats Alien. */
     const selections = {
         "alien": "spaceship",
         "spaceship": "astronaut",
@@ -27,48 +36,95 @@ function playRound(playerSelection, computerSelection) {
         return "Wrong input";
 
     if (playerSelection === computerSelection)
-        return "Tie";
+        return "It's a tie! The cosmic forces are perfectly balanced, as all things should be.";
     else if (selections[playerSelection] === computerSelection)
-        return "You win";             // if the corresponding value is computer's selection, the player wins
+        return "Congratulations! You emerged victorious in the Galactic Showdown!";  // if the corresponding value is computer's selection, the player wins
     else
-        return "You lose";
+        return "Oh no! Better luck next time in your cosmic battles.";
 }
 
+
+/**
+ * Handles what happens upon clicking
+ * @param event a click
+ */
 function handleClick(event) {
     const button = event.target;
-    let playerSelection;
+    let value;
+    let message;
 
     switch (button.id) {
         case "alienButton":
-            playerSelection = "alien";
+            value = "alien";
+            message = getRandomMessage(value);
             break;
         case "spaceshipButton":
-            playerSelection = "spaceship";
+            value = "spaceship";
+            message = getRandomMessage(value);
             break;
         case "astronautButton":
-            playerSelection = "astronaut";
+            value = "astronaut";
+            message = getRandomMessage(value);
             break;
-        default:
-            playerSelection = "unknown";
     }
 
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
+    const computerChoice = getComputerChoice();
+    const result = playRound(value, computerChoice);
     const resultContainer = document.getElementById("resultContainer");
-    resultContainer.textContent = result;
+
+    resultContainer.innerHTML = `You chose: ${value}<br>
+                                  ${message}<br>`;
+    // adding a delay of 3 seconds before showing the full result
+    setTimeout(() => {
+        resultContainer.innerHTML += `Your opponent chose: ${computerChoice}<br>
+                                       ${result}`;
+    }, 3000);
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock, paper or scissors?");
-        const computerSelection = getComputerChoice();
-        alert("Computer's choice: " + computerSelection);
-        alert(playRound(playerSelection, computerSelection));
+
+/**
+ * An object containing messages when a certain type is chosen
+ * @type {{spaceship: string[], astronaut: string[], alien: string[]}}
+ */
+const messages = {
+    alien: [
+        "You have chosen the Alien! Prepare to astound your opponents with extraterrestrial technology.",
+        "The Alien stands ready to defend your honor! Let its advanced tech lead you to triumph.",
+        "May the Alien's advanced technology guide your path to victory in this galactic duel.",
+        "The Alien awaits your command! Show your foes the might of extraterrestrial forces."
+    ],
+    spaceship: [
+        "You've selected the Spaceship! Embark on an epic cosmic journey across the galaxies.",
+        "As you choose the Spaceship, get ready to soar through the cosmos and unleash its interstellar power.",
+        "With the Spaceship as your ally, traverse the vastness of space and outmaneuver your opponents.",
+        "You've chosen the Spaceship, the epitome of cosmic exploration. Let the stars be your guide!"
+    ],
+    astronaut: [
+        "You've become the Astronaut! Prepare for a daring space odyssey and reach for the stars.",
+        "With the Astronaut as your avatar, embark on a cosmic adventure and defy the unknown.",
+        "You're the fearless space traveler! Show the universe your resourcefulness in the Galactic Showdown.",
+        "As the Astronaut, your fate is among the stars. Soar high and conquer the cosmos in style."
+    ]
+};
+
+
+/**
+ * Randomly chooses the messages from the object above
+ * @param type alien, spaceship or astronaut
+ * @returns {*|string} message to display
+ */
+function getRandomMessage(type) {
+    const messageList = messages[type.toLowerCase()];
+    if (!messageList) {
+        return "Invalid choice";
     }
+    return messageList[Math.floor(Math.random() * messageList.length)];
 }
+
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
     button.addEventListener("click", handleClick);
-})
-//game();
+});
+
+
